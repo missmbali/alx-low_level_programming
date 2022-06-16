9da9c8c3f1e7d656d5ae99a72d8c83b88c106b96
missmbali <missmbalisithole@gmail.com>
2022-06-16 07:25:35 -0700

    file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,7 +8,17 @@
 * Return: a string  
 */
 char *_strcat(char *dest, char *src)
+{
+      int len = 0, i;
 
-      strcat(dest, src);
+      while (dest[len])
+	len++;
+
+      for ( i = 0; src[i] !=0 i++)
+      {
+	dest[len] = src[i];
+        len += 1;
+      }      
+      dest[len] = '\0';
       return (dest);
 }
